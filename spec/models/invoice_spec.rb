d6c4b1b47ba83a75912ee9488f538f02d5aa048e
Otto Henrique <ottohenrique@gmail.com>
2022-01-29 13:48:14 -0300

    specs(invoice) more explicity descriptions in test suite

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  context 'validations' do
+  context 'with failing validations' do
     subject(:invoice) { described_class.new }
 
     before { invoice.valid? }
 
     it { expect(invoice).to_not be_valid }
-    it { expect(invoice.errors[:number]).to be_present }
-    it { expect(invoice.errors[:total_value]).to be_present }
+
+    it 'tests required fields' do
+      expect(invoice.errors.messages).to include(number: ["can't be blank"])
+      expect(invoice.errors.messages).to include(total_value: ["can't be blank"])
+    end
   end
 end
