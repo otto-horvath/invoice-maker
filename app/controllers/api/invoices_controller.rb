b9ae1770d0238576ee08ecc4928df4b62d0ce77f
Otto Henrique <ottohenrique@gmail.com>
2022-01-29 15:44:31 -0300

    feat(invoice) basic resource controller and specs

---
+++ app/controllers/api/invoices_controller.rb
@@ -0,0 +1,22 @@
+class Api::InvoicesController < Api::ApplicationController
+  def index
+    render json: { invoices: Invoice.all }
+  end
+
+  def show
+    render json: Invoice.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    head :not_found
+  end
+
+  def create
+    invoice = Invoice.create(invoice_params)
+    render json: invoice, status: :created
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:number, :total_value, :origin, :destiny, :issued_at)
+  end
+end
